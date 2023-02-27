b81fd5ba5492a7827983d3dfbb1a1343280d0ed8
Muziks1 <126173873+Muziks1@users.noreply.github.com>
2023-02-27 10:23:16 +0300

    Update helpers.py

--- strings/helpers.py
+++ strings/helpers.py
@@ -7,169 +7,79 @@
 #
 # All rights reserved.
 
-HELP_1 = """✅**<u>Admin Commands:</u>**
+HELP_1 = """✅ <u>𝖬𝗎̈𝗓𝗂𝗄 𝖪𝗈𝗆𝗎𝗍𝗅𝖺𝗋 :</u>
 
-**c** stands for channel play.
+➡️ **Şarkı & Video İndirmek İçin , @MusicVideoTRBot 'u kullanın ...**
 
-/pause or /cpause - Pause the playing music.
-/resume or /cresume- Resume the paused music.
-/mute or /cmute- Mute the playing music.
-/unmute or /cunmute- Unmute the muted music.
-/skip or /cskip- Skip the current playing music.
-/stop or /cstop- Stop the playing music.
-/shuffle or /cshuffle- Randomly shuffles the queued playlist.
-/seek or /cseek - Forward Seek the music to your duration
-/seekback or /cseekback - Backward Seek the music to your duration
-/restart - Restart bot for your chat .
-
-
-✅<u>**Specific Skip:**</u>
-/skip or /cskip [Number(example: 3)] 
-    - Skips music to a the specified queued number. Example: /skip 3 will skip music to third queued music and will ignore 1 and 2 music in queue.
-
-✅<u>**Loop Play:**</u>
-/loop or /cloop [enable/disable] or [Numbers between 1-10] 
-    - When activated, bot loops the current playing music to 1-10 times on voice chat. Default to 10 times.
-
-✅<u>**Auth Users:**</u>
-Auth Users can use admin commands without admin rights in your chat.
-
-/auth [Username] - Add a user to AUTH LIST of the group.
-/unauth [Username] - Remove a user from AUTH LIST of the group.
-/authusers - Check AUTH LIST of the group."""
-
-
-HELP_2 = """✅<u>**Play Commands:**</u>
-
-Available Commands = play , vplay , cplay
-
-ForcePlay Commands = playforce , vplayforce , cplayforce
-
-**c** stands for channel play.
-**v** stands for video play.
-**force** stands for force play.
-
-/play or /vplay or /cplay  - Bot will start playing your given query on voice chat or Stream live links on voice chats.
-
-/playforce or /vplayforce or /cplayforce -  **Force Play** stops the current playing track on voice chat and starts playing the searched track instantly without disturbing/clearing queue.
-
-/channelplay [Chat username or id] or [Disable] - Connect channel to a group and stream music on channel's voice chat from your group.
-
-
-✅**<u>Bot's Server Playlists:</u>**
-/playlist  - Check Your Saved Playlist On Servers.
-/deleteplaylist - Delete any saved music in your playlist
-/play  - Start playing Your Saved Playlist from Servers."""
-
-
-HELP_3 = """✅<u>**Bot Commands:**</u>
-
-/stats - Get Top 10 Tracks Global Stats, Top 10 Users of bot, Top 10 Chats on bot, Top 10 Played in a chat etc etc.
-
-/sudolist - Check Sudo Users of Yukki Music Bot
-
-/lyrics [Music Name] - Searches Lyrics for the particular Music on web.
-
-/song [Track Name] or [YT Link] - Download any track from youtube in mp3 or mp4 formats.
-
-/player -  Get a interactive Playing Panel.
-
-**c** stands for channel play.
-
-/queue or /cqueue- Check Queue List of Music."""
-
-HELP_4 = """✅<u>**Extra  Commands:**</u>
-/start - Start the Music Bot.
-/help  - Get Commands Helper Menu with detailed explanations of commands.
-/ping- Ping the Bot and check Ram, Cpu etc stats of Bot.
-
-✅<u>**Group Settings:**</u>
-/settings - Get a complete group's settings with inline buttons
-
-🔗 **Options in Settings:**
-
-1️⃣ You can set **Audio Quality** you want to stream on voice chat.
-
-2️⃣ You can set **Video Quality** you want to stream on voice chat.
-
-3️⃣ **Auth Users**:- You can change admin commands mode from here to everyone or admins only. If everyone, anyone present in you group will be able to use admin commands(like /skip, /stop etc)
-
-4️⃣ **Clean Mode:** When enabled deletes the bot's messages after 5 mins from your group to make sure your chat remains clean and good.
-
-5️⃣ **Command Clean** : When activated, Bot will delete its executed commands (/play, /pause, /shuffle, /stop etc) immediately.
-
-6️⃣ **Play Settings:**
-
-/playmode - Get a complete play settings panel with buttons where you can set your group's play settings. 
-
-<u>Options in playmode:</u>
-
-1️⃣ **Search Mode** [Direct or Inline] - Changes your search mode while you give /play mode. 
-
-2️⃣ **Admin Commands** [Everyone or Admins] - If everyone, anyone present in you group will be able to use admin commands(like /skip, /stop etc)
-
-3️⃣ **Play Type** [Everyone or Admins] - If admins, only admins present in group can play music on voice chat."""
-
-HELP_5 = """🔰**<u>ADD & REMOVE SUDO USERS :</u>**
-/addsudo [Username or Reply to a user]
-/delsudo [Username or Reply to a user]
-
-🛃**<u>HEROKU:</u>**
-/usage - Dyno Usage.
-
-🌐**<u>CONFIG VARS:</u>**
-/get_var - Get a config var from Heroku or .env.
-/del_var - Delete any var on Heroku or .env.
-/set_var [Var Name] [Value] - Set a Var or Update a Var on heroku or .env. Seperate Var and its Value with a space.
-
-🤖**<u>BOT COMMANDS:</u>**
-/reboot - Reboot your Bot. 
-/update - Update Bot.
-/speedtest - Check server speeds
-/maintenance [enable / disable] 
-/logger [enable / disable] - Bot logs the searched queries in logger group.
-/get_log [Number of Lines] - Get log of your bot from heroku or vps. Works for both.
-/autoend [enable|disable] - Enable Auto stream end after 3 mins if no one is listening.
-
-📈**<u>STATS COMMANDS:</u>**
-/activevoice - Check active voice chats on bot.
-/activevideo - Check active video calls on bot.
-/stats - Check Bots Stats
-
-⚠️**<u>BLACKLIST CHAT FUNCTION:</u>**
-/blacklistchat [CHAT_ID] - Blacklist any chat from using Music Bot
-/whitelistchat [CHAT_ID] - Whitelist any blacklisted chat from using Music Bot
-/blacklistedchat - Check all blacklisted chats.
-
-👤**<u>BLOCKED FUNCTION:</u>**
-/block [Username or Reply to a user] - Prevents a user from using bot commands.
-/unblock [Username or Reply to a user] - Remove a user from Bot's Blocked List.
-/blockedusers - Check blocked Users Lists
-
-👤**<u>GBAN FUNCTION:</u>**
-/gban [Username or Reply to a user] - Gban a user from bot's served chat and stop him from using your bot.
-/ungban [Username or Reply to a user] - Remove a user from Bot's gbanned List and allow him for using your bot
-/gbannedusers - Check Gbanned Users Lists
-
-🎥**<u>VIDEOCALLS FUNCTION:</u>**
-/set_video_limit [Number of Chats] - Set a maximum Number of Chats allowed for Video Calls at a time. Default to 3 chats.
-/videomode [download|m3u8] - If download mode is enabled, Bot will download videos instead of playing them in M3u8 form. ByDefault to M3u8. You can use download mode when any query doesnt plays in m3u8 mode.
-
-⚡️**<u>PRIVATE BOT FUNCTION:</u>**
-/authorize [CHAT_ID] - Allow a chat for using your bot.
-/unauthorize [CHAT_ID] - Disallow a chat from using your bot.
-/authorized - Check all allowed chats of your bot.
-
-🌐**<u>BROADCAST FUNCTION:</u>**
-/broadcast [Message or Reply to a Message] - Broadcast any message to Bot's Served Chats.
-
-<u>options for broadcast:</u>
-**-pin** : This will pin your message 
-**-pinloud** : This will pin your message with loud notification
-**-user** : This will broadcast your message to the users who have started your bot.
-**-assistant** : This will broadcast your message from assistant account of your bot.
-**-nobot** : This will force your bot to not broadcast message
-
-**Example:** `/broadcast -user -assistant -pin Hello Testing`
-
+» /vplay > 𝖵𝗂𝖽𝖾𝗈 𝗈𝗒𝗇𝖺𝗍 .
+» /oynat > 𝖲𝖺𝗋𝗄𝗂𝗒𝗂 𝗈𝗒𝗇𝖺𝗍 .
+» /durdur > 𝖲𝖺𝗋𝗄𝗂𝗒𝗂 𝖽𝗎𝗋𝖽𝗎𝗋 .
+» /devam > 𝖲𝖺𝗋𝗄𝗂𝗒𝗂 𝗌𝗎𝗋𝖽𝗎𝗋 .
+» /atla > 𝖲𝖺𝗋𝗄𝗂𝗒𝗂 𝖺𝗍𝗅𝖺 .
+» /son > 𝖲𝖺𝗋𝗄𝗂𝗒𝗂 𝗌𝗈𝗇𝗅𝖺𝗇𝖽𝗂𝗋 .
+» /karistir > 𝖲𝖺𝗋𝗄𝗂 𝗅𝗂𝗌𝗍𝖾𝗌𝗂𝗇𝗂 𝗄𝖺𝗋𝗂𝗌𝗍𝗂𝗋 .
+» /tekrarla > 𝖲𝖺𝗋𝗄𝗂𝗒𝗂 𝗍𝖾𝗄𝗋𝖺𝗋𝗅𝖺 .
+» /ileri > 𝖲𝖺𝗋𝗄𝗂𝗒𝗂 𝗂𝗅𝖾𝗋𝗂 𝗌𝖺𝗋 .
+» /seekback > 𝖲𝖺𝗋𝗄𝗂𝗒𝗂 𝗀𝖾𝗋𝗂𝗒𝖾 𝗌𝖺𝗋 .
+» /playforce > 𝖲ı𝗋𝖺𝗒ı 𝖻𝗈𝗓𝗆𝖺𝖽𝖺𝗇 𝗂𝗌𝗍𝖾𝖽𝗂𝗀̆𝗂𝗇𝗂𝗓 𝖲̧𝖺𝗋𝗄ı𝗒ı 𝗈𝗒𝗇𝖺𝗍ı𝗋 .
+» /vplayforce > 𝖲ı𝗋𝖺𝗒ı 𝖻𝗈𝗓𝗆𝖺𝖽𝖺𝗇 𝗂𝗌𝗍𝖾𝖽𝗂𝗀̆𝗂𝗇𝗂𝗓 𝖵𝗂𝖽𝖾𝗈𝗒𝗎 𝗈𝗒𝗇𝖺𝗍ı𝗋 .
+» /playlist > 𝖯𝗅𝖺𝗒𝗅𝗂𝗌𝗍𝗂𝗇𝗂𝗓𝗂 𝖮𝗒𝗇𝖺𝗍𝗂𝗋 .
+» /deleteplaylist > 𝖯𝗅𝖺𝗒𝗅𝗂𝗌𝗍𝗂𝗇𝗂𝗓𝗂 𝖲𝗂𝗅𝖾𝗋 .
+» /restart > 𝖡𝗈𝗍𝗎 𝖸𝖾𝗇𝗂𝖽𝖾𝗇 𝖡𝖺𝗌̧𝗅𝖺𝗍ı𝗋.
+» /reload > 𝖠𝖽𝗆𝗂𝗇 𝖫𝗂𝗌𝗍𝖾𝗌𝗂𝗇𝗂 𝖦𝗎𝗇𝖼𝖾𝗅𝗅𝖾 .
+» /auth > 𝖪𝗎𝗅𝗅𝖺𝗇ı𝖼ı𝗒𝖺 𝖸𝖾𝗍𝗄𝗂 𝖵𝖾𝗋𝗂𝗋 .
+» /unauth > 𝖪𝗎𝗅𝗅𝖺𝗇ı𝖼ı 𝖸𝖾𝗍𝗄𝗂𝗌𝗂𝗇𝗂 𝖠𝗅ı𝗋 .
+» /lang > 𝖡𝗈𝗍 𝗂𝖼̧𝗂𝗇 𝖻𝗂𝗋 𝖽𝗂𝗅 𝖻𝖾𝗅𝗂𝗋𝗅𝖾𝗒𝗂𝗇 .
 """
+
+HELP_2 = """✅ <u>𝖪𝖺𝗇𝖺𝗅 𝖮𝗒𝗇𝖺𝗍𝗆𝖺 :</u>
+
+◽ 𝖡𝗈𝗍𝗎 𝗄𝖺𝗇𝖺𝗅𝖺 𝖾𝗄𝗅𝖾𝗒𝗂𝗉 𝖸𝗈̈𝗇𝖾𝗍𝗂𝖼𝗂 𝗒𝖺𝗉ı𝗇, 𝖪𝖺𝗇𝖺𝗅 𝗂𝖽'𝗌𝗂𝗇𝗂 𝖺𝗅ı𝗉 𝗄𝖺𝗇𝖺𝗅ı𝗇 𝖻𝖺𝗀̆𝗅ı 𝗈𝗅𝖽𝗎𝗀̆𝗎 𝗀𝗋𝗎𝖻𝖺 𝗀𝗂𝗋𝗂𝗉 /channelplay ( 𝖪𝖺𝗇𝖺𝗅 𝗂𝖽 ) 𝗒𝖺𝗓ı𝗇 . 𝖳𝖺𝖻𝗂 𝖻𝗎𝗇𝗅𝖺𝗋ı 𝗄𝖺𝗇𝖺𝗅 𝗌𝖺𝗁𝗂𝖻𝗂 𝗒𝖺𝗉𝗆𝖺𝗅ı , 𝗄𝗈𝗆𝗎𝗍𝗅𝖺𝗋ı 𝖽𝖺 𝗌𝖾𝗌 𝗒𝖾𝗍𝗄𝗂𝗌𝗂 𝗈𝗅𝖺𝗇 𝗄𝗂𝗌̧𝗂𝗅𝖾𝗋 𝗄𝗎𝗅𝗅𝖺𝗇ı𝗋 .
+
+» /cplay > 𝖲𝖺𝗋𝗄𝗂𝗒𝗂 𝗈𝗒𝗇𝖺𝗍 .
+» /cpause > 𝖲𝖺𝗋𝗄𝗂𝗒𝗂 𝖽𝗎𝗋𝖽𝗎𝗋 .
+» /cresume > 𝖲𝖺𝗋𝗄𝗂𝗒𝗂 𝗌𝗎𝗋𝖽𝗎𝗋 .
+» /cskip > 𝖲𝖺𝗋𝗄𝗂𝗒𝗂 𝖺𝗍𝗅𝖺 .
+» /cend > 𝖲𝖺𝗋𝗄𝗂𝗒𝗂 𝗌𝗈𝗇𝗅𝖺𝗇𝖽𝗂𝗋 .
+» /cshuffle > 𝖲𝖺𝗋𝗄𝗂 𝗅𝗂𝗌𝗍𝖾𝗌𝗂𝗇𝗂 𝗄𝖺𝗋𝗂𝗌𝗍𝗂𝗋 .
+» /cloop > 𝖲𝖺𝗋𝗄𝗂𝗒𝗂 𝗍𝖾𝗄𝗋𝖺𝗋𝗅𝖺 .
+» /cseek > 𝖲𝖺𝗋𝗄𝗂𝗒𝗂 𝗂𝗅𝖾𝗋𝗂 𝗌𝖺𝗋 .
+» /cseekback > 𝖲𝖺𝗋𝗄𝗂𝗒𝗂 𝗀𝖾𝗋𝗂𝗒𝖾 𝗌𝖺𝗋 .
+» /cvplay > 𝖵𝗂𝖽𝖾𝗈 𝗈𝗒𝗇𝖺𝗍 .
+» /cplayforce > 𝖲ı𝗋𝖺𝗒ı 𝖻𝗈𝗓𝗆𝖺𝖽𝖺𝗇 𝗂𝗌𝗍𝖾𝖽𝗂𝗀̆𝗂𝗇𝗂𝗓 𝖲̧𝖺𝗋𝗄ı𝗒ı 𝗈𝗒𝗇𝖺𝗍ı𝗋 .
+» /cvplayforce > 𝖲ı𝗋𝖺𝗒ı 𝖻𝗈𝗓𝗆𝖺𝖽𝖺𝗇 𝗂𝗌𝗍𝖾𝖽𝗂𝗀̆𝗂𝗇𝗂𝗓 𝖵𝗂𝖽𝖾𝗈𝗒𝗎 𝗈𝗒𝗇𝖺𝗍ı𝗋 .
+"""
+
+
+HELP_3 = """✅<u>**𝖪𝖺𝗇𝖺𝗅 :**</u>
+• 𝖫𝖾𝗍'𝗌 𝖦𝗈 => [𝖲𝗍𝖺𝗋 𝖡𝗈𝗍 𝖪𝖺𝗇𝖺𝗅](t.me/StarBotKanal)
+ 
+"""
+
+
+HELP_4 = """✅<u>**𝖲𝖺𝗁𝗂𝗉 :**</u>
+• 𝖫𝖾𝗍'𝗌 𝖦𝗈 => [𝖲 𝖠 𝖧 𝖨 𝖯](t.me/Hayiboo)
+ 
+"""
+HELP_5 = """** 🔰<u>Sudo Komutlar :</u>
+
+» /addsudo [ Kullanıcı Kimliği ]
+» /delsudo [ Kullanıcı Kimliği ]
+» /sudolist [ listeyi görüntüle ]
+» /reboot  -> [ botu yeniden başlatır ]
+» /logger  -> [ log grubu içindir ]
+» /reklam  -> [ reklam verir ]
+» /vars  -> [ bottan detayli bilgi verir ]
+» /limit -> [ video limiti ayarlar ]
+» /blacklistchat  -> [ grubu yasaklar ]
+» /whitelistchat -> [ grub yasağı kaldırır ]
+» /blacklistedchat [ yasaklı listeyi gösterir ]
+» /activevoice  -> [ aktif sesli gruplar ]
+» /activevideo  -> [ aktif videolu gruplar ]
+» /block  -> [ kullanıcıyı Yasaklar ]
+» /unblock  -> [ yasağı Kaldırır]
+» /blocklist  -> [ yasaklı listeyi gösterir ]
+» /gban  -> [ kullanıcıyı grublardan banlar ]
+» /ungban  -> [ kullanıcı banını kaldırır ]
+» /gbanlist  -> [ banlananlar listesini gösterir ]
+**"""
