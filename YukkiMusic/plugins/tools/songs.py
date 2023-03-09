779df6a4c8b41978fbe2b6dc191d1873c7748acb
Muziks1 <126173873+Muziks1@users.noreply.github.com>
2023-03-10 01:33:41 +0300

    Update songs.py

--- YukkiMusic/plugins/tools/songs.py
+++ YukkiMusic/plugins/tools/songs.py
@@ -83,7 +83,7 @@ def bul(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"**▷ ᴘᴀʀᴄ̧ᴀ : {title[:35]}\n▷ sᴜ̈ʀᴇ : {duration}\n\n➠ ᴛᴀʟᴇᴘ : {message.from_user.first_name}\n\n   🌟[  ʀᴇsᴍɪ ᴋᴀɴᴀʟ  ](https://t.me/MuzikTRKanal)🌟**"
+        rep = f"**▷ ᴘᴀʀᴄ̧ᴀ : {title[:35]}\n▷ sᴜ̈ʀᴇ : {duration}\n\n➠ ᴛᴀʟᴇᴘ : {message.from_user.first_name}**"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
