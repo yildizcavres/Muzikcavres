e10b9d124ebfee860f5a6e176cda6352cfd048df
Muziks1 <126173873+Muziks1@users.noreply.github.com>
2023-03-10 00:29:45 +0300

    Update songs.py

--- YukkiMusic/plugins/tools/songs.py
+++ YukkiMusic/plugins/tools/songs.py
@@ -38,7 +38,6 @@ from pyrogram.types import (
 )
 
 SONG_COMMAND = get_command("SONG_COMMAND")
-VSONG_COMMAND = get_command("VSONG_COMMAND")
 
 
 @app.on_message(
@@ -104,57 +103,5 @@ def bul(client, message):
     try:
         os.remove(audio_file)
         os.remove(thumb_name)
-    except Exception as e:
-        print(e)
-
-
-@app.on_message(
-    filters.command(VSONG_COMMAND)
-    & filters.group
-    & ~filters.edited
-    & ~BANNED_USERS
-)
-async def vsong(client, message):
-    ydl_opts = {
-        "format": "best",
-        "keepvideo": True,
-        "prefer_ffmpeg": False,
-        "geo_bypass": True,
-        "outtmpl": "%(title)s.%(ext)s",
-        "quite": True,
-    }
-    query = " ".join(message.command[1:])
-    try:
-        results = YoutubeSearch(query, max_results=1).to_dict()
-        link = f"https://youtube.com{results[0]['url_suffix']}"
-        title = results[0]["title"][:40]
-        thumbnail = results[0]["thumbnails"][0]
-        thumb_name = f"{title}.jpg"
-        thumb = requests.get(thumbnail, allow_redirects=True)
-        open(thumb_name, "wb").write(thumb.content)
-        results[0]["duration"]
-        results[0]["url_suffix"]
-        results[0]["views"]
-        message.from_user.mention
-    except Exception as e:
-        print(e)
-    try:
-        msg = await message.reply("• **ᴠɪᴅᴇᴏ ᴀʀᴀɴɪʏᴏʀ ...**")
-        with YoutubeDL(ydl_opts) as ytdl:
-            ytdl_data = ytdl.extract_info(link, download=True)
-            file_name = ytdl.prepare_filename(ytdl_data)
-    except Exception as e:
-        return await msg.edit(f"• **ᴠɪᴅᴇᴏ ʙᴜʟᴜɴᴀᴍᴀᴅɪ ...**")
-    preview = wget.download(thumbnail)
-    await msg.edit("• **ᴠɪᴅᴇᴏ ɪɴᴅɪʀɪʟɪʏᴏʀ ...**")
-    await message.reply_video(
-        file_name,
-        duration=int(ytdl_data["duration"]),
-        thumb=preview,
-        caption=ytdl_data["title"],
-    )
-    try:
-        os.remove(file_name)
-        await msg.delete()
     except Exception as e:
         print(e)
